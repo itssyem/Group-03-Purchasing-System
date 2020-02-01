d4b44bd62aa9405adf683267ded7abc0e4a9bd17
rscaerzx <rscaerzx98@gmail.com>
2020-02-01 17:27:01 +0800

    Add project files.

---
+++ Purchasing System/env1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\USER\Desktop\Purchasing System t1920\Purchasing System\env1\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
